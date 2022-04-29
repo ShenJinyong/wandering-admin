2338c78e669f02a260f09347f883f5e62db16360
ShenJinyong <438217638@qq.com>
2022-04-29 23:51:34 +0800

    【优化】动态路由

--- src/components/admin/SideMenu/SideMenu.js
+++ src/components/admin/SideMenu/SideMenu.js
@@ -13,6 +13,52 @@ import './SideMenu.css'
 const { Sider } = Layout
 
 export default function SideMenu() {
+
+  const menuList = [
+    {
+      key: '/home',
+      icon: <UserOutlined />,
+      label: '首页',
+    },
+    {
+      icon: <UserOutlined />,
+      label: '用户管理',
+      children:[
+        {
+          key: '2',
+          icon: <UserOutlined />,
+          label: '全部用户',
+        },
+        {
+          key: '3',
+          icon: <UserOutlined />,
+          label: '增加用户',
+        }
+      ]
+    },
+    {
+      icon: <VideoCameraOutlined />,
+      label: '权限管理',
+      children:[
+        {
+          key: '4',
+          icon: <VideoCameraOutlined />,
+          label: '角色列表',
+        },
+        {
+          key: '5',
+          icon: <VideoCameraOutlined />,
+          label: '权限列表',
+        }
+      ]
+    },
+    {
+      key: '/friend',
+      icon: <UploadOutlined />,
+      label: '友谊外链',
+    },
+
+  ]
   return (
     <Sider trigger={null} collapsible >
           <div className="logo">流浪所后台管理系统</div>
@@ -20,23 +66,7 @@ export default function SideMenu() {
             theme="dark"
             mode="inline"
             defaultSelectedKeys={['1']}
-            items={[
-              {
-                key: '1',
-                icon: <UserOutlined />,
-                label: 'nav 1',
-              },
-              {
-                key: '2',
-                icon: <VideoCameraOutlined />,
-                label: 'nav 2',
-              },
-              {
-                key: '3',
-                icon: <UploadOutlined />,
-                label: 'nav 3',
-              },
-            ]}
+            items={menuList}
           />
         </Sider>
   )
